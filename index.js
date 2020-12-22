d53dfe1785a5a289cf940345e8aee5410a7cd25d
rumikoacopa <rumikorios@gmail.com>
2020-12-22 00:42:34 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+let divide = () => 2000/100;
+let square = x => x * x;
+let add = (x,y) => x + y;
+//let add = (parameter1, parameter2) ////parameter list
+//=> parameter1 + parameter2 //param //function
+//add(2,3)
+//
+//let sum = (parameter1, parameter2) //=> {
+//    console.log(`Adding ${parameter1}//`);
+//    console.log(`Adding ${parameter2}//`);
+//    return parameter1 + parameter2;
+//}
+//sum(1,2)
