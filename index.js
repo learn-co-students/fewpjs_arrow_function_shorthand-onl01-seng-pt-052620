27254182a85ced75cf26744230f7d2db5504e2e6
Jorge04 <Jorge196@users.noreply.github.com>
2021-01-09 14:47:54 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+let divide = function() {
+    return 2000 / 100;
+}
+
+let square = x => x ** 2;
+
+let add = (a, b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+}
+
+add(3,4)
